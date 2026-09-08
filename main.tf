bf524fc02c8469f432aee209704281d0b21e323f
sujeeth29 <srsujeethkrishna@gmail.com>
2026-09-08 14:06:18 +0530

    deploying infra on to aws

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "cognito_user_pool" {
-  source                        = "./modules/cognito"
-  cognito_user_pool_name        = var.cognito_user_pool_name
-  cognito_user_pool_domain      = var.cognito_user_pool_domain
-  cognito_user_pool_client_name = var.cognito_user_pool_client_name
-}
+# module "cognito_user_pool" {
+#   source                        = "./modules/cognito"
+#   cognito_user_pool_name        = var.cognito_user_pool_name
+#   cognito_user_pool_domain      = var.cognito_user_pool_domain
+#   cognito_user_pool_client_name = var.cognito_user_pool_client_name
+# }
 
 
 module "project_vpc" {
@@ -15,9 +15,9 @@ module "project_vpc" {
   env                              = var.env
 }
 
-module "public_server"{
-  source = "./modules/ec2"
-  env = var.env
-}
+# module "public_server"{
+#   source = "./modules/ec2"
+#   env = var.env
+# }
 
  
